55a54635bde787c6e5e4545a61e69c2a7e8c5412
严雪强 <yanxueqiang@yanxueqiangdeMacBook-Pro.local>
2023-02-21 14:14:32 +0800

    feat: 初始化

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -3,6 +3,14 @@
 // type 指提交的类型
 // subject 指提交的摘要信息
 // <type>: <subject>
+// 常用type值如下
+// feat: 添加新功能
+// fix: 修复bug
+// chore: 一些不影响功能的更改
+// docs: 专指文档的修改
+// pref: 性能方面的优化
+// refactor: 代码重构
+// test: 添加一些测试代码
 module.exports = {
   extends: ["@commitlint/config-conventional"]
 };
